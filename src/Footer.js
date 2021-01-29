b14d9953355c7501fecf12ff7c2bce7146854eb1
Alexander <46192425+AlexPan555@users.noreply.github.com>
2021-01-29 22:03:05 +0200

    fix text to Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,10 @@ function Footer() {
     return(
         <div className='footer'>
             <div className='footer_contacts'>
-               <span>©  2021</span> <span>By Pantchenco Alexander</span>   <i>lordpan555@gmail.com</i>  <span> https://github.com/AlexPan555</span>
+               <span>©  2021</span> <span>By Alex Pan</span>
+                <i>lordpan555@gmail.com</i>
+            <a href='https://github.com/AlexPan555' target="_blank">
+                <span> https://github.com/AlexPan555</span></a>
             </div>
         </div>
     )
