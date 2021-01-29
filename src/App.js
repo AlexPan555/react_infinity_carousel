9bd0030ed3f7a2532c643cb58ec72f63bce5437a
Alexander <46192425+AlexPan555@users.noreply.github.com>
2021-01-29 21:39:54 +0200

    add Footer.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './carousel.css'
 import MyCarousel from './Carousel'
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='app_body'>
         <MyCarousel />
       </div>
+        <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <div className='footer'>
+            <div className='footer_contacts'>
+                <span>By Pantchenco Alexander</span>   <i>lordpan555@gmail.com</i>  <span> https://github.com/AlexPan555</span>
+            </div>
+        </div>
+    )
+}
+export default  Footer
