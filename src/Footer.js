3467d5337086a02ed4d09e11be542ab3321e412f
Alexander <46192425+AlexPan555@users.noreply.github.com>
2021-01-29 21:47:48 +0200

    add text to Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return(
         <div className='footer'>
             <div className='footer_contacts'>
-                <span>By Pantchenco Alexander</span>   <i>lordpan555@gmail.com</i>  <span> https://github.com/AlexPan555</span>
+               <span>©  2021</span> <span>By Pantchenco Alexander</span>   <i>lordpan555@gmail.com</i>  <span> https://github.com/AlexPan555</span>
             </div>
         </div>
     )
